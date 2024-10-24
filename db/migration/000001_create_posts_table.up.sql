37a66a47c6d319440d9c33cec31521a2d0bed0f2
Fekinox <williamowenfowlk@gmail.com>
2024-10-24 13:37:07 -0400

    Modify database spec

--- db/migration/000001_create_posts_table.up.sql
+++ db/migration/000001_create_posts_table.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS posts (
     id bigserial PRIMARY KEY,
-    filename text NOT NULL,
-    deletion_key text NOT NULL,
-    hash text NOT NULL,
+    filename text,
+    deletion_key text,
+    hash text,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_filename UNIQUE (filename),
+    CONSTRAINT unique_hash UNIQUE(hash)
 );
